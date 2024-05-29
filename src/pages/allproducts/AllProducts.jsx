1eed206a2596700d7454fc19e11ed4ecc86ac955
Monu 2 <monupal057@gmail.com>
2024-05-30 01:57:34 +0530

    first commit

---
+++ src/pages/allproducts/AllProducts.jsx
@@ -0,0 +1,11 @@
+//import React from 'react'
+
+function AllProducts() {
+  return (
+    <div>
+      AllProduct
+    </div>
+  )
+}
+
+export default AllProducts
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+//import React from 'react'
+import  Layout from '../home/Home'
+
+function Home() {
+  return (
+    <Layout>
+      Home
+    </Layout>
+  )
+}
+
+export default Home
