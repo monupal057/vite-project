b9e746085d7218d8b5e47950c94cc674b3429e30
Monu pal <76943034+monupal057@users.noreply.github.com>
2024-06-01 00:46:52 +0530

    Add files via upload

---
+++ src/context/data/myState.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+
+//import React from 'react'
+import { useState } from "react";
+import MyContext from "./myContext"
+
+function MyState(props) {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = " rgb(17,24,30)"
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = "white"
+    }
+  }
+    
+  return (
+    <MyContext.Provider value={{mode, toggleMode}}>
+      {props.children}
+    </MyContext.Provider>
+  )
+}
+
+export default MyState;
