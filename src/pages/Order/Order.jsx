41df74d81366a922a1f2f5a5152d92929e9a4c4c
Monu pal <76943034+monupal057@users.noreply.github.com>
2024-06-01 00:29:46 +0530

    Update Order.jsx

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,10 +1,18 @@
 //import React from 'react'
 
+import { useContext } from "react"
+import Layout from "../../components/layout/Layout"
+import myContext from "../../context/data/myContext"
+
 function Order() {
+  const context = useContext(myContext)
+  const {name,rollno} = context;
   return (
-    <div>
-      order
-    </div>
+    <Layout>
+      Order
+      <h1>Name : {name}</h1>
+      <h1>Rollno : {rollno}</h1>
+    </Layout>
   )
 }
 
