6e6a4c0aa2e74f66fc17b3e0f590aa9d88f8fc3f
Monu pal <76943034+monupal057@users.noreply.github.com>
2024-06-01 00:30:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,27 @@
-// //import React from 'react'
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-// import Home from './pages/home/Home'
-// import Order from './pages/Order/Order'
-// import Cart from './pages/cart/Cart'
-// import Dashboard from './pages/admin/dashboard/Dashboard'
-// import NoPage from './pages/nopage/NoPage';
-
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Home/>} />
-//         <Route path="/order" element={<Order/>} />
-//         <Route path="/cart" element={<Cart/>} />
-//         <Route path="/dashboard" element={<Dashboard/>} />
-//         <Route path="/*" element={<NoPage/>} />
-//       </Routes>
-//     </Router>
-
-
-//   )
-// }
-
-// export default App
-
-
 //import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Home from './pages/home/Home'
+import Order from './pages/order/Order'
+import Cart from './pages/cart/Cart'
+import Dashboard from "./pages/admin/dashboard/Dashboard"
+import NoPage from "./pages/nopage/Nopage"
+import MyState from "./context/data/myState"
 
-import Home from './pages/home/Home';
-import Order from './pages/order/Order';
-import Cart from './pages/cart/Cart';
-import Dashboard from './pages/admin/dashboard/Dashboard';
-import NoPage from './pages/nopage/NoPage';
 
 function App() {
   return (
+    <MyState>
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>} />
+         <Route path="/" element={<Home/>} />
         <Route path="/order" element={<Order/>} />
         <Route path="/cart" element={<Cart/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/*" element={<NoPage/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />  
+         <Route path="/*" element={<NoPage/>} />
       </Routes>
     </Router>
+    </MyState>
   )
 }
 
-export default App;
+export default App
