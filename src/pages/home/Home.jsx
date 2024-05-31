9f9310d6e01432067ef9cc87bdadc19cda9ebc85
Monu pal <76943034+monupal057@users.noreply.github.com>
2024-06-01 00:29:10 +0530

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
 //import React from 'react'
-import  Layout from '../home/Home'
+import { useContext } from 'react'
+import Layout from '../../components/layout/Layout'
+import myContext from '../../context/data/myContext'
 
 function Home() {
+  const  context = useContext (myContext)
+  console.log(context);
+
   return (
     <Layout>
       Home
